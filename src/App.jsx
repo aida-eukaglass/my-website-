463b9dffe8012b4a84ae45a5a9cd6584ccf3d9e0
aida-eukaglass <aidahuo130@gmail.com>
2025-08-12 22:14:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
-import pdfjsWorker from 'pdfjs-dist/legacy/build/pdf.worker.min.js';
+import * as pdfjsLib from 'pdfjs-dist';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.min.js';
 
 // 显式指定 worker 源
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
